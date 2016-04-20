c87ca1e0e9d7bcabce6ec65dcb1bb3ef0fa42219
Artem <mak.art.nik@mail.ru>
2016-04-20 19:54:32 +0400

    add IdataStorage and DataStorage2

---
+++ Система аэронавигации/AirNavSystem/AirNavSystem/DataStorage2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace AirNavSystem
+{
+    class DataStorage2 : IDataStorage<Bitmap>, IDataStorage<AlgorithmParams>
+    {
+        AlgorithmParams[] IDataStorage<AlgorithmParams>.Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        Bitmap[] IDataStorage<Bitmap>.Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataStorage<AlgorithmParams>.Store(params AlgorithmParams[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataStorage<Bitmap>.Store(params Bitmap[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
