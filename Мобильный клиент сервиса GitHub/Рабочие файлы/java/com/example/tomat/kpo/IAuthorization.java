49e0facdcd9dfe63890ce7ddbd7174fb57499a83
Tomat-ПК\Tomat <Sergey_Kryutsin@epam.com>
2016-04-21 21:41:57 +0400

    добавил исходники

---
+++ Мобильный клиент сервиса GitHub/Рабочие файлы/java/com/example/tomat/kpo/IAuthorization.java
@@ -0,0 +1,8 @@
+package com.example.tomat.kpo;
+
+/**
+ * Created by Tomat on 21.04.2016.
+ */
+public interface IAuthorization {
+    void authorizeUser(User user);
+}
---
+++ Мобильный клиент сервиса GitHub/Рабочие файлы/java/com/example/tomat/kpo/IGUI.java
@@ -0,0 +1,12 @@
+package com.example.tomat.kpo;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Tomat on 21.04.2016.
+ */
+public interface IGUI {
+    Fragment drawer = null;
+    Fragment registrationForm = null;
+    IRepoGui repoGui = null;
+}
