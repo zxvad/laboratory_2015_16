1de6944ac09bd5172fa793b5cc575f7a217abbf4
Dorby <nekron-l@yandex>
2016-04-22 14:24:05 +0400

    Добавлен модуль соединения с сервером

---
+++ Автоматизация учета результатов прохождения диагностики детьми с особенностями умственного развития/Рабочие файлы/Server/Server/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketListener
+{
+
+    // ВХодящая информация от клиента
+    public static string data = null;
+
+    public static void StartListening()
+    {
+        // Буффер данных для входящих данных
+        byte[] bytes = new Byte[1024];
+
+        IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+        IPAddress ipAddress = ipHostInfo.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+
+        Socket listener = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+
+
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection...");
+
+                Socket handler = listener.Accept();
+                data = null;
+
+
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+
+
+                Console.WriteLine("Text received : {0}", data);
+
+
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                handler.Send(msg);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+
+    }
+
+    public static int Main(String[] args)
+    {
+        StartListening();
+        return 0;
+    }
+}
